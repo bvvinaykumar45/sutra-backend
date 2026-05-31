a755ea8bf08e2b67b3cfeec4f0a5270ff4590b18
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-05-31 10:55:29 +0530

    Setup Atlas MongoDB connection via Mongoose

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import "dotenv/config";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Sutra Backend App is Up & Listening on port ${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Sutra Backend App is Up & Listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error", error);
+    process.exit(1);
+  });
