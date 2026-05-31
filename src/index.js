9a572f8438e56a49f92d7af70919837cbe84c806
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-05-31 10:31:10 +0530

    Setup Express Server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "dotenv/config";
+import expres from "express";
 
-console.log("Initializing Backend Coding.");
+const app = expres();
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Sutra Backend App is Up & Listening on port ${port}`);
+})
