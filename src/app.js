820005d307dacc0c6cf93a3ab3a5d54b2f98790a
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-05-31 10:34:13 +0530

    Basic Express Request Middleware Configurations

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+// Basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Welcome to Sutra Backend");
