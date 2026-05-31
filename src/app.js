ec28cb154fa463fc5040a00422604094476cde70
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-05-31 10:37:58 +0530

    Setup CORS Middleware Configurations

--- src/app.js
+++ src/app.js
@@ -8,6 +8,16 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+// CORS configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
+
 app.get("/", (req, res) => {
   res.send("Welcome to Sutra Backend");
 });
