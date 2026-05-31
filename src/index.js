b5c55cd3fb8cc131974f6547a45821bd28f3064a
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-05-31 10:33:21 +0530

    Extracting Express App Logic to a separate file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import "dotenv/config";
-import expres from "express";
+import app from "./app.js";
 
-const app = expres();
 const port = process.env.PORT || 8000;
-
-app.get('/', (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Sutra Backend App is Up & Listening on port ${port}`);
-})
+});
