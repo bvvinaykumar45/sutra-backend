996e449ae47446551ce2c624283c556604ad085e
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-06-01 21:06:45 +0530

    Update: Mispelt Route file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 
-import healthCheckRouter from "./routes/health-check.routes.js";
+import healthCheckRouter from "./routes/health-check.route.js";
 
 const app = express();
 
