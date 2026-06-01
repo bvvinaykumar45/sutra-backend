fa7aa8d1efd22374bb7ea596900abea5c408da30
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-06-01 21:05:35 +0530

    Define Promise Wrapper on Request Handler

--- src/controllers/health-check.controller.js
+++ src/controllers/health-check.controller.js
@@ -1,16 +1,13 @@
 import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthCheck = (req, res) => {
-  try {
-    res
-      .status(200)
-      .json(
-        new ApiResponse(200, { message: "Server is running", status: "pass" }),
-      );
-  } catch (error) {
-    throw new ApiError(500, error.message, [], error.stack);
-  }
-};
+const healthCheck = asyncHandler(async (req, res) => {
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, { message: "Server is running", status: "pass" }),
+    );
+});
 
 export { healthCheck };
