177611b83b8c6bd4be5d5c69a9ea8873062e17e7
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-06-02 21:43:21 +0530

    Define & Setup Sending Emails Logic

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,3 +1,48 @@
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+// Nodemailer Transporter -> used to send
+const transporter = nodemailer.createTransport({
+  host: process.env.MAILTRAP_SMTP_HOST,
+  port: process.env.MAILTRAP_SMTP_PORT,
+  auth: {
+    user: process.env.MAILTRAP_SMTP_USER,
+    pass: process.env.MAILTRAP_SMTP_PASS,
+  },
+});
+
+// Mailgen Object used for mail body generation
+const mailGenerator = new Mailgen({
+  theme: "default",
+  product: {
+    name: "Sutra - Project Manager",
+    link: "https://sutra.com",
+  },
+});
+
+const senEmail = async (options) => {
+  const plainTextEmail = mailGenerator.generatePlaintext(
+    options.mailgenContent,
+  );
+  const htmlEmail = mailGenerator.generate(options.mailgenContent);
+
+  const mail = {
+    from: "mail.project-manager@sutra.com",
+    to: options.email,
+    subject: options.subject,
+    text: plainTextEmail,
+    html: htmlEmail,
+  };
+
+  try {
+    const info = await transporter.sendMail(mail);
+    console.log("Message sent: %s", info.messageId);
+  } catch (error) {
+    console.error("Email service failed.");
+    console.error({ message: error.message, stack: error.stack });
+  }
+};
+
 const emailVerificationContent = (userName, verificationUrl) => {
   return {
     body: {
@@ -38,4 +83,4 @@ const forgotPasswordContent = (userName, passwordResetUrl) => {
   };
 };
 
-export { emailVerificationContent, forgotPasswordContent };
+export { emailVerificationContent, forgotPasswordContent, sendEmail };
