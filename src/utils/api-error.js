1cd23292794b9375b0517dd1c582e64d49aa7254
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-06-04 21:40:26 +0530

    Update ApiError message property to be enumerable

--- src/utils/api-error.js
+++ src/utils/api-error.js
@@ -8,9 +8,17 @@ class ApiError extends Error {
     super(message);
     this.statusCode = statusCode;
     this.data = null;
-    this.message = message;
     this.success = false;
     this.errors = errors;
+
+    // as message is set by calling super the Error class makes it enumerable fasle which in turn doesn't show up in JSON.stringify conversion as it requires properties to be enumerable
+    // To make message property enumerable
+    Object.defineProperty(this, "message", {
+      value: message,
+      enumerable: true, // ← this is what makes it show up in JSON
+      writable: true,
+      configurable: true,
+    });
 
     if (stack) {
       this.stack = stack;
