d2b875cc2e80adb78fdddf83c9b6e48f8ac55713
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-06-05 21:55:39 +0530

    Setup User login route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 
 import { userRegisterValidator } from "../validators/auth.validator.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { registerUser } from "../controllers/auth.controller.js";
+import { login, registerUser } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
+router.route("/login").post(login);
 
 export default router;
