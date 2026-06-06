36a49639d70af374048245d82aebc08e941f828c
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-06-06 21:18:53 +0530

    Define & Setup get currentuser Controller & Route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -143,4 +143,10 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "User logged out successfully."));
 });
 
-export { registerUser, login, logoutUser };
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"));
+});
+
+export { registerUser, login, logoutUser, getCurrentUser };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,6 +7,7 @@ import {
 import { validate } from "../middlewares/validator.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
+  getCurrentUser,
   login,
   logoutUser,
   registerUser,
@@ -16,6 +17,9 @@ const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+
+// secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 
 export default router;
